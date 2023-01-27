a03924aa630e9d0bfcad99e7ba44fdde2fe2174d
Nathan Pope <nathan.pope@bbc.co.uk>
2023-01-27 14:46:51 +0000

    WIP working on adding keyboard events
    Co-authored-by: Caitlin Kirby <CaitlinKirby@users.noreply.github.com>
    Co-authored-by: Thomas Moreno Cooper <TMoCo@users.noreply.github.com>
    Co-authored-by: Jack-E-Boreham <Jack-E-Boreham@users.noreply.github.com>
    Co-authored-by: Mikey-Mulgrew <Mikey-Mulgrew@users.noreply.github.com>
    Co-authored-by: Brad Phipps <BradleyPhipps@users.noreply.github.com>

--- src/App.js
+++ src/App.js
@@ -1,59 +1,66 @@
 import React, { useState, useEffect } from "react";
 import Board from "./components/Board";
 import "./App.css";
+import Sound from "react-sound";
+//import Drake from "./drake-quackline-bling.mp3";
 
-const MAX_ROW = 3;
+const mp3_url = "./components/drake-quackline-bling.mp3";
+
+const MAX_ROW = 24;
 const MAX_COL = 10;
 
 const matrix = (rows, columns) => {
   return Array.from({ length: rows }, () => new Array(columns).fill(""));
 };
 
+const withinFrame = ([row, col]) =>
+  row >= 0 && row < MAX_ROW && col >= 0 && col < MAX_COL;
+
 const App = () => {
   const [boardState, setBoardState] = useState(matrix(MAX_ROW, MAX_COL));
+  let shapePosition = [0, 0];
 
-  const [shapePosition, setShapePosition] = useState([0, 0]);
-
-  const fillCell = (row, col, val) => {
-    boardState[row][col] = val;
+  const updateBoard = (oldPos, newPos) => {
+    const newBoardState = boardState;
+    if (withinFrame(oldPos)) newBoardState[oldPos[0]][oldPos[1]] = "";
+    if (withinFrame(newPos)) newBoardState[newPos[0]][newPos[1]] = "X";
+    return newBoardState;
   };
 
-  //fillCell(0, 0, "X");
+  useEffect(() => {
+    window.addEventListener("keydown", (e) => {
+      if (e.key === "Enter") {
+        console.log("You pressed Enter");
+      }
+      if (e.key === "ArrowLeft") {
+        updateBoard(shapePosition, [shapePosition[0], shapePosition[1] - 1]);
+      }
+      if (e.key === "ArrowRight") {
+        updateBoard(shapePosition, [shapePosition[0], shapePosition[1] + 1]);
+      }
+    });
+  });
 
   useEffect(() => {
     const intervalId = setInterval(() => {
-      setBoardState(() => {
-        const newBoardState = boardState;
-        console.log(shapePosition);
-
-        if (newBoardState[0] - 1) {
-          newBoardState[shapePosition[0] - 1][shapePosition[1]] = "";
-        }
-
-        // draw current cell
-        newBoardState[shapePosition[0]][shapePosition[1]] = "X";
-
-        // paint next cell position
-        if (shapePosition[0] + 1 !== MAX_ROW) {
-          //newBoardState[shapePosition[0] + 1][shapePosition[1]] = "X";
-        }
-
-        return newBoardState;
-      });
+      setBoardState(updateBoard());
 
       // check that new position is valid
-
-      if (shapePosition[0] + 1 === MAX_ROW) {
-        setShapePosition([0, shapePosition[1]]);
+      if (
+        shapePosition[0] + 1 === MAX_ROW ||
+        boardState[shapePosition[0] + 1][shapePosition[1]] === "X"
+      ) {
+        shapePosition.curent = [0, shapePosition[1]];
       } else {
-        setShapePosition([shapePosition[0] + 1, shapePosition[1]]);
+        shapePosition.current = [shapePosition[0] + 1, shapePosition[1]];
       }
     }, 1000);
     return () => clearInterval(intervalId);
-  }, [boardState, shapePosition]);
+  }, [boardState, shapePosition, updateBoard]);
 
   return (
     <div className="App">
+      <Sound url={mp3_url} playStatus={Sound.status.PLAYING} loop={true} />
       <h1>TETRIS DUCKS 🦆🦆🦆</h1>
       <Board boardState={boardState} />
     </div>
--- src/components/Board.js
+++ src/components/Board.js
@@ -2,11 +2,16 @@
 // Whenever this is updated, it rerenders (using state) and updtes the visuals onscreen
 import React from "react";
 import styled from "styled-components";
+import { GiDuck } from "react-icons/gi";
 
 const Cell = styled.div`
   height: 1.5rem;
   width: 1.5rem;
-  background-color: ${({ isFilled }) => (isFilled ? "green" : "grey")};
+  background-color: ${({ isFilled }) =>
+    isFilled ? "rgba(0, 255, 0, 0.3)" : "rgba(192, 192, 192, 0.3)"};
+  display: inline-block;
+  justify-content: center;
+  align-items: center;
   border: 1px solid white;
   border-radius: 0.25rem;
 `;
@@ -26,7 +31,9 @@ const Board = ({ boardState }) => {
       {boardState.map((row, rowIndex) => (
         <Row key={`Row-${rowIndex}`}>
           {row.map((cell, colIndex) => (
-            <Cell key={`Cell-${colIndex}`} isFilled={cell && true} />
+            <Cell key={`Cell-${colIndex}`} isFilled={cell && true}>
+              {cell && true && <GiDuck />}
+            </Cell>
           ))}
         </Row>
       ))}
